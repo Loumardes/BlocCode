6803f13c34c31a2719fa9df16149b2070de33982
Loumardes <40787527+Loumardes@users.noreply.github.com>
2019-05-29 17:22:38 +0200

    Test script of the last version

---
+++ Scripts/0.1.1_script.py
@@ -0,0 +1,10 @@
+from Bloccode_1_1 import *
+
+new_objective("anobjectivename")
+
+
+
+set_tag_functions()
+
+write_in(temp_folder = "main", temp_name = "init", tag_function = "load",
+         commands = "say Hello world")

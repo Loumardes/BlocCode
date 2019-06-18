38784e04dc6a61651ad440e027295f5796e69087
Loumardes <40787527+Loumardes@users.noreply.github.com>
2019-06-18 06:43:45 +0200

    Update 0.1.1_script.py

--- Scripts/0.1.1_script.py
+++ Scripts/0.1.1_script.py
@@ -1,10 +1,9 @@
-from Bloccode_1_1 import *
+from Bloccode_0_1_2 import *
 
 new_objective("anobjectivename")
 
 
-
-set_tag_functions()
-
 write_in(temp_folder = "main", temp_name = "init", tag_function = "load",
          commands = "say Hello world")
+
+set_tag_functions()
